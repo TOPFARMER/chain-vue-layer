c705dfbe845e64aad4705144b54db8544487365a
chashao <461006220@qq.com>
2019-04-06 17:19:23 +0800

    再尝试部署

--- src/main.js
+++ src/main.js
@@ -2,11 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
-const feathers = require("@feathersjs/client");
-const io = require("socket.io-client");
+// const feathers = require("@feathersjs/client");
+// const io = require("socket.io-client");
 
-const client = feathers();
-const socket = io('http://192.168.178.157:3030');
+// const client = feathers();
+// const socket = io('http://192.168.178.157:3030');
 
 
 client.configure(feathers.socketio(socket));
